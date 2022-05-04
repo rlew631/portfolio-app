58fd5aa2871b2357adc867a28c032101bdfc741c
Ryan Lewis <rlewis94@gmail.com>
2022-05-04 13:27:39 -0700

    renaming

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import Intro from './components/intro.jsx';
 import About from './components/about.jsx';
 import Portfolio from './components/portfolio.jsx';
 import Contact from './components/contact.jsx';
-import BackToTop from './components/back-top.jsx';
+import BackToTop from './components/footer.jsx';
 import Preloader from './components/preloader';
 
 
