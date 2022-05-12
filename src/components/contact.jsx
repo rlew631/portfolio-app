cec88b58edf502aae56986e6c188f991808d5a1e
Ryan Lewis <rlewis94@gmail.com>
2022-05-12 09:50:38 -0700

    updated second form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -139,7 +139,78 @@ class Contact extends React.Component {
               </form>
             </div>
             <div id="Work Opportunities" className="tabcontent" style={{display: "none"}}>
-              hahahaha
+            <form
+                action="https://formspree.io/f/xzbkyopb"
+                method="POST"
+                className="contactForm"
+                onSubmit={this.submitForm}
+              >
+                <div id="sendmessage">
+                  Your message has been sent. Thank you!
+                </div>
+                <div id="errormessage"></div>
+                <div className="row">
+                  <div className="col-md-12 mb-3">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        data-rule="minlen:4"
+                        data-msg="Please enter at least 4 chars"
+                      />
+                      <div className="validation"></div>
+                    </div>
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="_replyto"
+                        id="email"
+                        placeholder="Your Email"
+                        data-rule="email"
+                        data-msg="Please enter a valid email"
+                      />
+                      <div className="validation"></div>
+                    </div>
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="_subject"
+                        id="subject"
+                        placeholder="Subject"
+                        data-rule="minlen:4"
+                        data-msg="Please enter at least 8 chars"
+                      />
+                      <div className="validation"></div>
+                    </div>
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <div className="form-group">
+                      <textarea
+                        className="form-control"
+                        name="message"
+                        rows="5"
+                        data-rule="required"
+                        data-msg="Please write something for us"
+                        placeholder="Message"
+                      ></textarea>
+                      <div className="validation"></div>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    {status === "SUCCESS" ? <button type="submit" className="button button-a button-big button-rouded">Your message has been sent!</button> : <button type="submitted" className="button button-a button-big button-rouded">Send Message</button>}
+                    {status === "ERROR" && <button type="failed" className="button button-a button-big button-rouded">There was an error, please contact me on LinkedIn</button>}
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
