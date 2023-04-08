ca7492db155898a0fd6878a226570f3c49a2d91b
ryan <rlew631@gmail.com>
2023-04-08 13:22:24 -0700

    fixed navbar height

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@
 //   reactStrictMode: true,
 // }
 
-const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
-
-module.exports = withCss(withPurgeCss());
-
 // module.exports = nextConfig
 module.exports = {
   // reactStrictMode: true,
