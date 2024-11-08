5747d6e25a91f18faddd901f0c83f76991224b80
ryan <rlew631@gmail.com>
2024-11-07 16:22:56 -0800

    added allmendelabs and updated deps

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
-import Scrollspy from 'react-scrollspy';
+import React, { useState, useEffect, useRef } from "react";
+import { Scrollspy } from "@makotot/ghostui";
 
-const Navbar = ():JSX.Element => {
+interface NavbarProps {
+  sectionRefs: React.RefObject<HTMLDivElement>[];
+}
+
+const Navbar = ({ sectionRefs }: NavbarProps): JSX.Element => {
   const [logoColor, setLogoColor] = useState("white");
   const [logoHeight, setLogoHeight] = useState(115);
-  const [toggleColor, setToggleColor] = useState('black');
-
-  const [navClass, setNavClass] = useState("navbar navbar-expand-md navbar-b navbar-trans fixed-top")
+  const [toggleColor, setToggleColor] = useState("black");
+  const [navClass, setNavClass] = useState(
+    "navbar navbar-expand-md navbar-b navbar-trans fixed-top"
+  );
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleScroll = () => {
@@ -14,58 +19,52 @@ const Navbar = ():JSX.Element => {
       setLogoColor("black");
       setLogoHeight(50);
       setToggleColor("#0078ff");
-      setNavClass("navbar navbar-expand-md navbar-b navbar-reduce fixed-top ")
+      setNavClass("navbar navbar-expand-md navbar-b navbar-reduce fixed-top");
     } else {
       setLogoColor("white");
       setLogoHeight(115);
       setToggleColor("black");
-      setNavClass("navbar navbar-expand-md navbar-b navbar-trans fixed-top")
+      setNavClass("navbar navbar-expand-md navbar-b navbar-trans fixed-top");
     }
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll); 
+    window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  });
+  }, []);
 
-  const handleNavClick = (e) => {
-    e.preventDefault();
-    const href = e.target.getAttribute("href");
-    const offsetTop = document.querySelector(href).offsetTop;
-    window.scrollTo({
-      top: offsetTop - 50,
-      behavior: "smooth"
-    });
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      window.scrollTo({
+        top: ref.current.offsetTop - 50,
+        behavior: "smooth",
+      });
+    }
   };
 
   const handleNavToggle = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const scrollToTop = () => {
-    window.scrollTo({top: 0,behavior: "smooth"})
-  }
-
   return (
     <nav className={navClass}>
-      <div className="container navbar-contents">
-        <a className="navbar-brand js-scroll" href="#">
-          <svg
-            width={175}
-            height={logoHeight}
-            preserveAspectRatio="xMinYMid"
-            className="logo"
-            fill={logoColor}
-            viewBox="0 0 45.9 30.8"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g strokeWidth=".265" aria-label="RL">
-                <path d="M11.7 5.7H6.3v8.1h5.6c5.5 0 5.5-8.1-.2-8.1zm18.5 25.1H21l-10.5-11H6.3v11H0V0h12.8c12.1 0 13.4 13.1 5.4 17.8z"/>
-                <path d="M45.9 30.8H25.8V0h7.4v24.8h12.7z"/>
-            </g>
-          </svg>
-        </a>
-        <button
+      <a className="navbar-brand js-scroll" href="#">
+        <svg
+          width={175}
+          height={logoHeight}
+          preserveAspectRatio="xMinYMid"
+          className="logo"
+          fill={logoColor}
+          viewBox="0 0 45.9 30.8"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g strokeWidth=".265" aria-label="RL">
+              <path d="M11.7 5.7H6.3v8.1h5.6c5.5 0 5.5-8.1-.2-8.1zm18.5 25.1H21l-10.5-11H6.3v11H0V0h12.8c12.1 0 13.4 13.1 5.4 17.8z"/>
+              <path d="M45.9 30.8H25.8V0h7.4v24.8h12.7z"/>
+          </g>
+        </svg>
+      </a>
+      <button
           id="nabar-toggler"
           className="navbar-toggler collapsed"
           type="button"
@@ -77,39 +76,66 @@ const Navbar = ():JSX.Element => {
           <span className="toggler-span" style={{backgroundColor:toggleColor, height:"0.2em", borderRadius:'01.em'}}/>
           {/* three bars for hamburger dropdown icon */}
         </button>
-        <div
-          className={`navbar-collapse collapse justify-content-end ${isMenuOpen ? 'show' : ''}`}
-          id="navbarDefault"
-        >
-          <Scrollspy
-            items={["home", "about", "work", "contact"]}
-            currentClassName="active"
-            offset={-50}
-            className="navbar-nav"
-            style={{paddingTop:'1em'}}
-          >
-            <li className="nav-item">
-              <a id="navbar-link" className="nav-link js-scroll" href="#" onClick={scrollToTop}>
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a id="navbar-link" className="nav-link js-scroll" href="#about" onClick={handleNavClick}>
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a id="navbar-link" className="nav-link js-scroll" href="#work" onClick={handleNavClick}>
-                Work
-              </a>
-            </li>
-            <li className="nav-item">
-              <a id="navbar-link" className="nav-link js-scroll" href="#contact" onClick={handleNavClick}>
-                Contact
-              </a>
-            </li>
-          </Scrollspy>
-        </div>
+      <div
+        className={`navbar-collapse collapse justify-content-end ${
+          isMenuOpen ? "show" : ""
+        }`}
+        id="navbarDefault"
+      >
+        <Scrollspy sectionRefs={sectionRefs}>
+          {({ currentElementIndexInViewport }) => (
+            <ul className="navbar-nav" style={{ paddingTop: "1em" }}>
+              <li
+                className={`nav-item ${
+                  currentElementIndexInViewport === 0 ? "active" : ""
+                }`}
+              >
+                <a
+                  className="nav-link js-scroll"
+                  onClick={() => scrollToSection(sectionRefs[0])}
+                >
+                  Home
+                </a>
+              </li>
+              <li
+                className={`nav-item ${
+                  currentElementIndexInViewport === 1 ? "active" : ""
+                }`}
+              >
+                <a
+                  className="nav-link js-scroll"
+                  onClick={() => scrollToSection(sectionRefs[1])}
+                >
+                  About
+                </a>
+              </li>
+              <li
+                className={`nav-item ${
+                  currentElementIndexInViewport === 2 ? "active" : ""
+                }`}
+              >
+                <a
+                  className="nav-link js-scroll"
+                  onClick={() => scrollToSection(sectionRefs[2])}
+                >
+                  Work
+                </a>
+              </li>
+              <li
+                className={`nav-item ${
+                  currentElementIndexInViewport === 3 ? "active" : ""
+                }`}
+              >
+                <a
+                  className="nav-link js-scroll"
+                  onClick={() => scrollToSection(sectionRefs[3])}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          )}
+        </Scrollspy>
       </div>
     </nav>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,52 @@
+import React, { useRef } from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 
-// import components
+// Import components
 import Navbar from '../components/navbar';
 import Intro from '../components/intro';
 import About from '../components/about';
 import Portfolio from '../components/portfolio';
 
-// lazy loading
+// Lazy loading
 const DynamicContact = dynamic(() => import('../components/contact'), {
   loading: () => <p>Loading...</p>,
-})
+});
 const DynamicFooter = dynamic(() => import('../components/footer'), {
   loading: () => <p>Loading...</p>,
-})
-
+});
 
 function Home(): JSX.Element {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const workRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const sectionRefs = [homeRef, aboutRef, workRef, contactRef];
+
   return (
     <>
       <Head>
-        <title>Ryan Lewis</title> 
+        <title>Ryan Lewis</title>
       </Head>
       <main>
-        <Navbar />
-        <Intro />
-        <About />
-        <Portfolio />
-        {/* <Contact /> */}
-        <DynamicContact/>
-        {/* <Footer /> */}
-        <DynamicFooter/>
+        <Navbar sectionRefs={sectionRefs} />
+        <div ref={homeRef} id="home">
+          <Intro />
+        </div>
+        <div ref={aboutRef} id="about">
+          <About />
+        </div>
+        <div ref={workRef} id="work">
+          <Portfolio />
+        </div>
+        <div ref={contactRef} id="contact">
+          <DynamicContact />
+        </div>
+        <DynamicFooter />
       </main>
     </>
-  )
+  );
 }
 
 export default Home;
